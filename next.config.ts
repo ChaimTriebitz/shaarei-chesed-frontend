1eef263c0835adebb6fd81fffbbc8a2625f86ae5
chaim <chaim.triebitz@bhbsdventures.com>
2025-04-23 17:49:55 +0300

    Refactor Next.js configuration: Simplify image handling by enabling unoptimized images and setting output to 'export'. Update build script in package.json to remove export step.

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256],
-    formats: ['image/avif', 'image/webp'],
-    minimumCacheTTL: 60 * 60 * 24, // 24 hours
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    unoptimized: false,
-    remotePatterns: [],
-  },
-  compress: true,
-  poweredByHeader: false,
-  reactStrictMode: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
-  experimental: {
-    optimizeCss: true,
-    optimizePackageImports: ['@/components'],
-  }
+   output: 'export',
+   images: {
+      unoptimized: true,
+   },
+   
 };
 
 export default nextConfig;
