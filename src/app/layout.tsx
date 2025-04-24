c0d98f39461bd7f0203b2d6c898fd204d3d30954
chaim <chaim.triebitz@bhbsdventures.com>
2025-04-24 09:29:52 +0300

    Add suppressHydrationWarning to body element in RootLayout for improved hydration handling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -69,6 +69,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         {children}
       </body>
